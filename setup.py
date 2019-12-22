147a0f1f203c1d82b0f661c1e660a26ab1fa24b3
Natalie Fearnley <nfearnley@gmail.com>
2019-12-22 16:59:38 -0500

    fixed script name

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setuptools.setup(
     ],
     entry_points={
         "console_scripts": [
-            "yiffx=yiffscraper.__main__:main"
+            "yiff=yiffscraper.__main__:main"
         ]
     }
 )
