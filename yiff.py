25ba841242b04055bfd3db4cb0f086fe0248ec67
Natalie Fearnley <nfearnley@gmail.com>
2019-12-22 00:29:14 -0500

    Change from using asyncio.run(main()) to loop.run_until_complete(main()) to allow the program to run with python >= 3.5 instead of requiring >= 3.7

--- yiff.py
+++ yiff.py
@@ -236,6 +236,8 @@ async def main():
     print("\nEnjoy ;)")
 
 
-# Main program.
+# Main program
 if __name__ == "__main__":
-    asyncio.run(main())
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    loop.close()
